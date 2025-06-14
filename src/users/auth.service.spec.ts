08d5d8dfa74c3e16ae4387c8cd7008e373cec9f8
betb9 <bender.etienne57@gmail.com>
2025-06-14 14:50:16 +0200

    Test authService to throws error for email exists on signup

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -37,5 +39,16 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        { id: 1, email: 'user@user.com', password: '1' } as User,
+      ]);
+
+    await expect(service.signup('user@user.com', '1')).rejects.toThrow(
+      BadRequestException,
+    );
   });
 });
